78c4711924c9156d5747b964179197b8124165a5
Daniil Borovkov <daniilborowkow@gmail.com>
2019-02-12 21:32:27 +0600

    Initial commit

---
+++ db-recent-posts-slideshow.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Db Recent Posts Slideshow
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     db-recent-posts-slideshow
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Db_Recent_Posts_Slideshow
+ */
+
+// Your code starts here.
